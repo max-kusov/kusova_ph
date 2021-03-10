187dacf155a21b5681a5c8354fbded3bda6d5280
Kusov <Nutsmax88@gmail.com>
2021-03-10 14:17:55 +0300

    action link in header

--- script.js
+++ script.js
@@ -1,68 +1,81 @@
-const burger = document.querySelector(".burger"),
-    burgerIcon = document.querySelector(".header__burger"),
-    exitBurger = document.querySelector(".burger__exit"),
-    linksBurger = document.querySelectorAll(".burger a"),
-    mediaQuery = window.matchMedia('(max-width: 768px)'),
-    sliderBtns = document.querySelectorAll(".slider__btn"),
-    pages = document.querySelectorAll(".slider__page"),
-    arrows = document.querySelectorAll(".slider__arrow");
+document.addEventListener("DOMContentLoaded", () => {
 
-let counter = 0;
+    const burger = document.querySelector(".burger"),
+        burgerIcon = document.querySelector(".header__burger"),
+        exitBurger = document.querySelector(".burger__exit"),
+        linksBurger = document.querySelectorAll(".burger a"),
+        mediaQuery = window.matchMedia('(max-width: 768px)'),
+        sliderBtns = document.querySelectorAll(".slider__btn"),
+        pages = document.querySelectorAll(".slider__page"),
+        arrowLeft = document.querySelector(".fa-chevron-left"),
+        arrowRight = document.querySelector(".fa-chevron-right");
 
-function handleTabletChange(e) {
-    if (e.matches) {
+    let counter = 0;
 
-    } else {
-        burger.style.cssText = "display: none";
-        exitBurger.style.cssText = "display: none";
+    function handleTabletChange(e) {
+        if (e.matches) {
 
+        } else {
+            burger.style.cssText = "display: none";
+            exitBurger.style.cssText = "display: none";
+
+        }
     }
-}
-mediaQuery.addListener(handleTabletChange);
-handleTabletChange(mediaQuery)
+    mediaQuery.addListener(handleTabletChange);
+    handleTabletChange(mediaQuery)
 
 
-burgerIcon.addEventListener("click", () => {
-    burger.style.cssText = "display: flex";
-    burgerIcon.style.cssText = "display: none";
-    exitBurger.style.cssText = "display: block";
+    burgerIcon.addEventListener("click", () => {
+        burger.style.cssText = "display: flex";
+        burgerIcon.style.cssText = "display: none";
+        exitBurger.style.cssText = "display: block";
+    })
+
+    exitBurger.addEventListener("click", () => {
+        burger.style.cssText = "display: none";
+        burgerIcon.style.cssText = "display: block";
+        exitBurger.style.cssText = "display: none";
+    })
+    function manualNav(item) {
+        linksBurger.forEach(link => {
+            link.style.cssText = "color: rgb(204, 203, 203)";
+        })
+        linksBurger[item].style.cssText = "color: #fff";
+    }
+
+    linksBurger.forEach((link, i) => {
+        link.addEventListener("click", () => {
+            manualNav(i);
+        })
+    })
+
+    function movePage(item) {
+        pages.forEach(page => {
+            page.classList.remove("active");
+        })
+        sliderBtns.forEach(btn => {
+            btn.classList.remove("active");
+        })
+        pages[item].classList.add("active");
+        sliderBtns[item].classList.add("active");
+    }
+
+    sliderBtns.forEach((btn, i) => {
+        btn.addEventListener("click", () => {
+            movePage(i);
+        })
+    })
+
+    arrowLeft.addEventListener("click", () => {
+        console.log("лево");
+    })
+    arrowRight.addEventListener("click", () => {
+        pages.forEach((page, i) => {
+            page.classList.remove("active");
+            i++;
+        })
+    })
+
 })
-
-exitBurger.addEventListener("click", () => {
-    burger.style.cssText = "display: none";
-    burgerIcon.style.cssText = "display: block";
-    exitBurger.style.cssText = "display: none";
-})
-function manualNav(item) {
-    linksBurger.forEach(link => {
-        link.style.cssText = "color: rgb(204, 203, 203)";
-    })
-    linksBurger[item].style.cssText = "color: #fff";
-}
-
-linksBurger.forEach((link, i) => {
-    link.addEventListener("click", () => {
-        manualNav(i);
-    })
-})
-
-function movePage(item) {
-    pages.forEach(page => {
-        page.classList.remove("active");
-    })
-    sliderBtns.forEach(btn => {
-        btn.classList.remove("active");
-    })
-    pages[item].classList.add("active");
-    sliderBtns[item].classList.add("active");
-}
-
-sliderBtns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        movePage(i);
-    })
-})
-
-
 
 
