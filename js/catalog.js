cacd6c752636fb40b781f7e81d37e5ac54393063
Kusov <Nutsmax88@gmail.com>
2021-04-08 15:20:01 +0300

    fixed modal window js

--- js/catalog.js
+++ js/catalog.js
@@ -96,4 +96,322 @@ const catalogList = [
         img: 'img/portfolio/anna/anna_20.jpg',
         alt: 'anna',
     },
+    // alina
+    {
+        id: 'alina',
+        img: 'img/portfolio/alina/1.jpg',
+        alt: 'alina',
+    },
+    {
+        id: 'alina',
+        img: 'img/portfolio/alina/2.jpg',
+        alt: 'alina',
+    },
+    {
+        id: 'alina',
+        img: 'img/portfolio/alina/3.jpg',
+        alt: 'alina',
+    },
+    {
+        id: 'alina',
+        img: 'img/portfolio/alina/4.jpg',
+        alt: 'alina',
+    },
+    {
+        id: 'alina',
+        img: 'img/portfolio/alina/5.jpg',
+        alt: 'alina',
+    },
+    {
+        id: 'alina',
+        img: 'img/portfolio/alina/6.jpg',
+        alt: 'alina',
+    },
+    {
+        id: 'alina',
+        img: 'img/portfolio/alina/7.jpg',
+        alt: 'alina',
+    },
+    {
+        id: 'alina',
+        img: 'img/portfolio/alina/8.jpg',
+        alt: 'alina',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_1.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_2.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_3.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_4.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_5.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_6.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_7.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_8.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_9.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_10.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_11.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_12.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_13.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_14.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_15.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_16.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_17.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_18.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_19.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_20.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_21.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_22.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_23.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_24.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_25.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_26.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_27.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_28.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_29.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_30.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'oksana',
+        img: 'img/portfolio/oksana/oksana_31.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_1.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_2.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_3.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_4.jpg',
+        alt: 'Oksana',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_5.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_6.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_7.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_8.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_9.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_10.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_11.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_12.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_13.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_14.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_15.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_16.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_17.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_18.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_19.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_20.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_21.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_22.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_23.jpg',
+        alt: 'tany',
+    },
+    {
+        id: 'tany',
+        img: 'img/portfolio/tany/tany_24.jpg',
+        alt: 'tany',
+    },
+
+
 ]
--- js/portfolio_window.js
+++ js/portfolio_window.js
@@ -3,20 +3,22 @@ document.addEventListener("DOMContentLoaded", () => {
         modal = document.querySelector(".modal"),
         btnExit = document.querySelector(".modal__exit");
 
-    let modalWindow = document.querySelector('.modal__table'),
-        modalImg = document.querySelectorAll('img');
-
+    let modalWindow = document.querySelector('.modal__table');
 
     itemsPortfolio.forEach(item => {
         item.addEventListener("click", () => {
+            let modalImg = modalWindow.querySelectorAll('img');
+
             modal.style.display = "block";
             document.body.style.overflow = "hidden";
 
+            let filterDate = item.dataset['name'];
+            console.log(filterDate);
 
-            catalogList.filter((img) => {
-                if (img.id == item.dataset['name']) {
-                    console.log('raboteet')
-
+            modalImg.forEach(img => {
+                img.style.display = 'block';
+                if (!img.classList.contains(filterDate)) {
+                    img.style.display = 'none';
                 }
             })
         })
@@ -42,3 +44,16 @@ document.addEventListener("DOMContentLoaded", () => {
     photo.render();
 });
 
+// nav.addEventListener('click', event => {
+//     if (event.target.tagName !== 'LI') return false;
+
+//     let filterDate = event.target.dataset['color'];
+//     console.log(filterDate);
+
+//     square.forEach(item => {
+//         item.style.display = 'block'
+//         if (!item.classList.contains(filterDate)) {
+//             item.style.display = 'none'
+//         }
+//     })
+// })
